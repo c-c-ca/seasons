b26957dffbc29d3c3d0693d0530c78032ada2063
Adam Carter <adam.clayton.carter@gmail.com>
2020-11-10 03:49:46 -0500

    Display user's latitude

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <div>App</div>;
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { lat: null };
+    }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            ({ coords: { latitude: lat } }) => this.setState({ lat }),
+            err => console.log(err)
+        );
+    }
+
+    render() {
+        return <div>Latitude: {this.state.lat}</div>;
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
